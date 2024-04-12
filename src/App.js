796b2f08b2f6991268617a8407b055e08a88ae0a
kiran5112 <kbijarane@gmail.com>
2024-04-12 13:57:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+Kiran is the best Girl        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
